7cc84ef98eecafa8e07dee33b5a37d07b58e9a79
Jeevanandam <jeevanandam770@gmail.com>
2024-08-20 16:04:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,18 +3,24 @@ class Solution {
     {
         int  [] res = new int[2];
         HashMap<Integer,Integer> map = new HashMap<>();
-        int i,val;
-        for (i=0;i<nums.length;i++)
+        int i, val;
+        
+        for (i = 0; i < nums.length; i++)
         {
-            val = target-nums[i];
-            if (map.containsKey(val))
+            val = target - nums[i]; // To find in the map to reach the target sum.
+            
+            if (map.containsKey(val)) // Check if this required value exists in the map.
             {
-                res[0]=i;
-                res[1]=map.get(val);
+                res[0] = i; // The current index (where we found the number).
+                
+                res[1] = map.get(val);                 
+                // The index of the previously stored number that together with the current number sums to the target.
                 return res;
             }
-            map.put(nums[i],i);
+            
+            map.put(nums[i], i);e map.
         }
+        
         return res;
     }
 }
